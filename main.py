6b22b798472f9966aaf73c922a1410fb0d5c4740
njmin <gazinurnurtdinov35@gmail.com>
2023-01-31 22:43:12 +0500

    Сделал более адекватную структуру и добавил комментов

--- main.py
+++ main.py
@@ -1,87 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
+import output_couples as oc
 
 link = "https://isu.ugatu.su/api/new_schedule_api/?schedule_semestr_id=222&WhatShow=1&student_group_id=1557"
 responce = requests.get(link).text
 soup = BeautifulSoup(responce, 'lxml')
 
 tbody_block = soup.find('tbody') #Тело таблицы там усе
-week_days = tbody_block.find_all('tr', 'noinfo dayheader' and 'dayheader') #Споисок со всеми днями неделями(пока не используется)
-
-
-'''
-#Получаем названия дней недели, максимально топорно и тупо конечно
-j = 0
-for i in week_days:
-    #print(week_days[j].find('td').text)
-    j+=1
-# check_wd = week_days[0].find_all('td')
-'''
-
-
-def ras_less(num, index, week): #метод для вывода собственно расписания
-    num_less = 0  # эта
-    last_time = 0  # и эта переменная введена для адекватного отображения списка
-
-    for k in range(num):
-        check_less = lesson[index].find_all('td')
-
-        # print(last_time, check_less[1].text)
-
-        if check_less[2].text == "Нет информации":#те случаи когда td имеет содержание "Нет информации"
-            num_less += 1
-            # Не может быть ситуации когда нет информации и повтор урока
-            # Так что, тут проверку делать не обязательно
-            # print(f"Пара №{num_less } - {check_less[1].text} - Pizda")
-            last_time = check_less[1].text
-        else:
-            if last_time != check_less[1].text:
-                num_less += 1
-                if str(week) in check_less[2].text:
-                    print(
-                        f"Пара №{num_less} - {check_less[1].text} #{check_less[4].text}# {check_less[3].text} - {check_less[5].text} - {check_less[6].text}")
-                    last_time = check_less[1].text
-                # else:
-                # Когда по уроку не "нет информации", а не совпадают по датам
-                # print(f"Пара №{num_less} - {check_less[1].text} - Bebra")
-                last_time = check_less[1].text
-                # num_less += 1
-            else:
-                if str(week) in check_less[2].text:
-                    print(
-                        f"Пара №{num_less} - {check_less[1].text} #{check_less[4].text}# {check_less[3].text} - {check_less[5].text} - {check_less[6].text}")
-                else:
-                    pass
-                last_time = check_less[1].text
-        index += 1
-
 lesson = tbody_block.find_all('tr')
-# check_less = tbody_block.find_all('td')
+check_lessik = tbody_block.find_all('td')
 
 week = input("Введи неделю: ")
 day_week = input("Введите день недели: ")
 
-check_lessik = tbody_block.find_all('td')
 
-if day_week == "Понедельник":
-    j = 0
-    for i in check_lessik:
+"""Штука высчитывает сколько строк с парами есть в этот день и передает их в функцию для вывода,
+Чтобы функция вывода не выходила за рамки вызываемого дня"""
+
+if day_week == "Понедельник": #Если чувак написал Понедельник
+    j = 0 #Переменная помогающая найти строку вызываего дня
+    for i in check_lessik: #Пербором находим нужный день
+
         if check_lessik[j].text == "Понедельник":
-            num = 1
-            index = 0
-            for i in check_lessik:
+            num = 1 #перемменая которая содержит количество строк с парами в этот день(везде разное)
+            index = 0 #переменная которая показывает функции вывода откуда(с какой позиции td) нужно начинать
+
+            for i in check_lessik: #Перебор строк для счета количества строк
                 # print(check_less[index].text)
-                if check_lessik[index].text == "" and (check_lessik[index-1].text == "" or check_lessik[index-1].text == "Нет информации"):
+
+                if check_lessik[index].text == "" and (check_lessik[index-1].text == "" or
+                                                       check_lessik[index-1].text == "Нет информации"):
                     index += 1
                     num += 1
-                elif check_lessik[index].text == "Вторник":
+                elif check_lessik[index].text == "Вторник": #Если дошел до строки Вторник остановись
                     break
                 else:
                     index += 1
         else:
             j+=1
     print(num)
-    ras_less(num, 0, week)
+    oc.ras_less(num, 0, week, lesson)
 
 elif day_week == "Вторник":
     j = 0
@@ -91,7 +49,8 @@ elif day_week == "Вторник":
             index = j
             for i in check_lessik:
                 # print(check_lessik[index].text)
-                if check_lessik[index].text == "" and (check_lessik[index - 1].text == "" or check_lessik[index - 1].text == "Нет информации"):
+                if check_lessik[index].text == "" and (check_lessik[index - 1].text == "" or
+                                                       check_lessik[index - 1].text == "Нет информации"):
                     index += 1
                     num += 1
                 elif check_lessik[index].text == "Среда":
@@ -101,14 +60,14 @@ elif day_week == "Вторник":
         else:
             j += 1
     print(num)
-    ras_less(num, 14, week)
+    oc.ras_less(num, 14, week)
 
 elif day_week == "Среда":
     index = 20
-    ras_less(index, week)
+    oc.ras_less(index, week)
 elif day_week == "Четверг":
     index = 34
-    ras_less(index, week)
+    oc.ras_less(index, week)
 elif day_week == "Пятница":
     index = 30
-    ras_less(index, week)
+    oc.ras_less(index, week)
---
+++ output_couples.py
@@ -0,0 +1,38 @@
+
+def ras_less(num, index, week, lesson): #метод для вывода собственно расписания
+
+    num_less = 0  # эта
+    last_time = 0  # и эта переменная введена для адекватного отображения списка
+
+    for k in range(num):
+        check_less = lesson[index].find_all('td')
+        # print(last_time, check_less[1].text)
+
+        if check_less[2].text == "Нет информации": #те случаи когда td имеет содержание "Нет информации"
+            num_less += 1
+            # Не может быть ситуации когда нет информации и повтор урока
+            # Так что, тут проверку делать не обязательно
+            # print(f"Пара №{num_less } - {check_less[1].text} - Pizda")
+            last_time = check_less[1].text
+        else:
+            if last_time != check_less[1].text:
+                num_less += 1
+                if str(week) in check_less[2].text:
+                    print(
+                        f"Пара №{num_less} - {check_less[1].text} #{check_less[4].text}# {check_less[3].text} - "
+                        f"{check_less[5].text} - {check_less[6].text}")
+                    last_time = check_less[1].text
+                # else:
+                # Когда по уроку не "нет информации", а не совпадают по датам
+                # print(f"Пара №{num_less} - {check_less[1].text} - Bebra")
+                last_time = check_less[1].text
+                # num_less += 1
+            else:
+                if str(week) in check_less[2].text:
+                    print(
+                        f"Пара №{num_less} - {check_less[1].text} #{check_less[4].text}# {check_less[3].text} -"
+                        f" {check_less[5].text} - {check_less[6].text}")
+                else:
+                    pass
+                last_time = check_less[1].text
+        index += 1
